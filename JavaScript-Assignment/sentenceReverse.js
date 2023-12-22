1194e52893500ec775c7a0a5088f625c00d479b4
ShubhamKPawar <97871705+ShubhamKPawar@users.noreply.github.com>
2023-12-22 23:23:29 +0530

    Add files via upload

---
+++ JavaScript-Assignment/sentenceReverse.js
@@ -0,0 +1,29 @@
+/*
+var input = prompt("Enter a sentence:");
+var output = reverseWords(input);
+console.log(output);
+*/
+
+var input = "This is a sunny day";
+var output = reverseWords(input);
+console.log(output);
+
+
+
+function reverseWords(sentence) {
+  var array = sentence.split(' ');
+  for (var i = 0; i < array.length; i++) {
+    array[i] = reverseString(array[i]);
+  }
+
+  var reverseSentence = array.join(' ');
+  return reverseSentence;
+}
+
+function reverseString(str) {
+  var reversed = '';
+  for (var i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
